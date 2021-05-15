c79f5dfa20a7e1236cd263e1fbce9f4b69859d99
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-05-15 17:44:33 -0500

    Se crearon el componente inicio navegacion y el principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavegacionComponent} from './navegacion/navegacion.component';
+import {AppComponent} from './app.component';
+import {InicioComponent} from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { BancoComponent } from './banco/banco.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    BancoComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
