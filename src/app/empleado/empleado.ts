d757d5c13f344c64c7448f3a748b85c3f208d374
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-05-26 10:16:04 -0500

    crear el modelo empleados en la clase empleado.ts

---
+++ src/app/empleado/empleado.ts
@@ -0,0 +1,9 @@
+export interface Empleado{
+  cedula: number;
+  nombre: string;
+  apellido: string;
+  telefono: number;
+  fechaNacimiento: Date;
+  correoElectronico: string;
+  idFormaDePago: number;
+}
