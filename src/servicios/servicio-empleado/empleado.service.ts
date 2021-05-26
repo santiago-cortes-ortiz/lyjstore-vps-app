2f8bb3abb736b410ab0713c53c00877b73c60fae
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-05-26 10:25:13 -0500

    listar empleados is reaady en el servicio

--- src/servicios/servicio-empleado/empleado.service.ts
+++ src/servicios/servicio-empleado/empleado.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {Banco} from '../../app/banco/banco';
+import {Empleado} from '../../app/empleado/empleado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  private urlBackendEmpleado: string = environment.apiBackendUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public listarEmpleados(): Observable<Empleado[]> {
+    return this.http.get<Empleado[]>(`${this.urlBackendEmpleado}/empleado/listar`);
+  }
+
 }
